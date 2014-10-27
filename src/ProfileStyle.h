eaecc7a68085acce41022ff66eef53a74f0d99b5
julianadenauer <github@julianadenauer.de>
2014-10-27 18:47:19 +0100

    cleanup in ProfileStyle

--- src/ProfileStyle.h
+++ src/ProfileStyle.h
@@ -16,24 +16,14 @@
 class ProfileStyle {
     
 public:
-    ProfileStyle();
-    void update();
-    void draw();
-    void dragging(int x, int y);
-    void released(int x, int y);
     void render(vector<GPoint> &points);
+    
     void setFill(bool fill);
     void toggleFill();
-    void clear();
-    void addPoint(GPoint p);
-    int getStrokeId(){return stroke_id;}
-    
     
 private:
-    bool fill;
-    vector<vector<GPoint> > points;
+    bool fill = true;
     ofPoint lastPoint;
-    int stroke_id;
 };
 
 
