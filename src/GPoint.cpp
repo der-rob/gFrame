4ad91e5bbf055d532e49484cadcc87acbe9b846c
julianadenauer <github@julianadenauer.de>
2014-11-17 18:53:30 +0100

    point (de)serialize now works with float values

--- src/GPoint.cpp
+++ src/GPoint.cpp
@@ -27,8 +27,8 @@ GPoint::GPoint(int x, int y, int finger_id, int color_r, int color_g, int color_
 void GPoint::serialize(string *s){
     s->clear();
     
-    s->append(ofToString((int) loc.x) + "," +
-              ofToString((int) loc.y) + "," +
+    s->append(ofToString(loc.x, 4) + "," +
+              ofToString(loc.y, 4) + "," +
               ofToString((int) finger_id) + "," +
               ofToString((int) color.r) + "," +
               ofToString((int) color.g) + "," +
@@ -43,7 +43,8 @@ void GPoint::serialize(string *s){
 bool GPoint::unserialize(string s){
     vector<string> list;
     
-    RegularExpression regEx("(-?[0-9]{1,4},){2}([0-9]{1,3},){6}([0-9]{1,10})"); // accept negative values on the first two arguments (position) and long numbers on the last (strokeid)
+//    RegularExpression regEx("(-?[0-9]{1,4},){2}([0-9]{1,3},){6}([0-9]{1,10})"); // accept negative values on the first two arguments (position) and long numbers on the last (strokeid)
+    RegularExpression regEx("(-?[0-1].[0-9]{0,4},){2}([0-9]{1,3},){6}([0-9]{1,10})"); // NEW: Deal with float values for x&y
     RegularExpression::Match match;
     int found = regEx.match(s, match);
     
@@ -53,7 +54,7 @@ bool GPoint::unserialize(string s){
             list.push_back(s.substr(0, pos));
             s = s.substr(pos+1);
         }
-        loc = ofVec2f(ofToInt(list[0]), ofToInt(list[1]));
+        loc = ofVec2f(ofToFloat(list[0]), ofToFloat(list[1]));
         this->finger_id = ofToInt(list[2]);
         this->color = ofColor(ofToInt(list[3]), ofToInt(list[4]), ofToInt(list[5]));
         this->type = ofToInt(list[6]);
